856499db3aaaf974c61cc0d6943cae84762170ff
Dingel, Brody <bdingel@hy-vee.com>
2022-04-19 06:43:23 -0500

    Add handler for GET movie

--- main.go
+++ main.go
@@ -8,8 +8,25 @@ import (
 	"net/http"
 )
 
-func movieHandler(w http.ResponseWriter, r *http.Request) {
+func moviesHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(movies)
+}
+
+func movieHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	for _, movie := range movies {
+		if movie.ID == id {
+			json.NewEncoder(w).Encode(movie)
+
+			return
+		}
+	}
+
+	w.WriteHeader(http.StatusNotFound)
+
+	w.Write([]byte(fmt.Sprintf("Movie with ID %s not found", id)))
 }
 
 type Movie struct {
@@ -29,8 +46,10 @@ var movies []Movie
 func main() {
 	r := mux.NewRouter()
 	movies = append(movies, Movie{ID: "1", Isbn: "12345", Title: "My first movie", Director: &Director{FirstName: "John", LastName: "Doe"}})
+	movies = append(movies, Movie{ID: "2", Isbn: "67890", Title: "My second movie", Director: &Director{FirstName: "Jane", LastName: "Doe"}})
 
-	r.HandleFunc("/movies", movieHandler).Methods("GET")
+	r.HandleFunc("/movies", moviesHandler).Methods("GET")
+	r.HandleFunc("/movie/{id}", movieHandler).Methods("GET")
 
 	fmt.Printf("Listening on port 8080...\n")
 	log.Fatal(http.ListenAndServe(":8080", r))
